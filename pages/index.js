3146aee76503cabd0ffb54d3238c2d03cc4b74b1
akash prasad <akashorasad2000@gmail.com>
2021-02-26 10:35:30 +0530

    test predict api

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useState } from "react";
 
+import axios from "axios";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -17,7 +18,7 @@ const Main = styled.main`
 	flex: 1;
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
+	/* justify-content: center; */
 	align-items: center;
 `;
 
@@ -45,12 +46,35 @@ const Footer = styled.footer`
 	}
 `;
 
+const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+`;
+
+const FileInput = styled.input`
+	display: none;
+`;
+
+const Button = styled.button`
+	background-color: #ccc;
+	margin-top: 2rem;
+`;
+
+const Label = styled.label`
+	background-color: #000;
+	color: #fff;
+	padding: 0.5rem 1rem;
+`;
+
 export default function Home() {
 	const [imagePreview, setImagePreview] = useState("");
 	const [imageToUpload, setImageToUpload] = useState(null);
+	const [freezeForm, setFreezeForm] = useState(false);
+	const [predictedImage, setPredictedImage] = useState("");
 
 	const handleInputChange = async e => {
 		const file = e.target.files[0];
+		if (!file) return;
 		setImageToUpload(file);
 		previewImageFile(file);
 	};
@@ -63,17 +87,39 @@ export default function Home() {
 		};
 	}
 
-	const uploadImage = () => {
+	const predictHandler = async () => {
+		try {
+			const res = await axios({
+				url: "http://008133b7ff53.ngrok.io/predict",
+				headers: {
+					"Context-Type": "application/json",
+				},
+				method: "POST",
+				data: {
+					url:
+						"https://res.cloudinary.com/dhqp2dd6b/image/upload/v1614309837/image-processing/mtvjctglxenopdprugyj.png",
+				},
+			});
+
+			console.log(res);
+			setPredictedImage(res.data.url);
+		} catch (error) {
+			console.log(error);
+		}
+		setFreezeForm(false);
+	};
+
+	const uploadImage = e => {
+		e.preventDefault();
+		setFreezeForm(true);
 		const formData = new FormData();
 		formData.append("file", imageToUpload);
 		formData.append("upload_preset", "dlyu4xhy");
 
-		fetch("https://api.cloudinary.com/v1_1/dhqp2dd6b/image/upload", {
+		axios("https://api.cloudinary.com/v1_1/dhqp2dd6b/image/upload", {
 			method: "POST",
-			body: formData,
-		})
-			.then(res => res.json())
-			.then(result => console.log(result));
+			data: formData,
+		}).then(result => predictHandler(result.data.secure_url));
 	};
 
 	return (
@@ -87,20 +133,32 @@ export default function Home() {
 				<Title>Forged Bot !!!!</Title>
 
 				<img src={imagePreview} />
-				<button onClick={uploadImage}>upload Image</button>
-				<form>
-					<input type='file' onChange={handleInputChange} />
-				</form>
-			</Main>
 
-			<Footer>
+				<fieldset disabled={freezeForm}>
+					<Form onSubmit={uploadImage} target='/search'>
+						<Label htmlFor='upload-img'>uploadd image</Label>
+						<FileInput
+							required
+							type='file'
+							id='upload-img'
+							onChange={handleInputChange}
+						/>
+						<Button type='submit'>verify</Button>
+					</Form>
+				</fieldset>
+				<img src={predictedImage} />
+			</Main>
+		</Container>
+	);
+}
+
+{
+	/* <Footer>
 				<a
 					href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
 					target='_blank'
 					rel='noopener noreferrer'>
 					Powered by <img src='/vercel.svg' alt='Vercel Logo' />
 				</a>
-			</Footer>
-		</Container>
-	);
+			</Footer> */
 }
