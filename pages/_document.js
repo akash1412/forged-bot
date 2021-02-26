454efc776f8ea4a37793c3249386dfcaa789ded1
akash prasad <akashorasad2000@gmail.com>
2021-02-26 21:29:29 +0530

    updated _docuemnt.js file

--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,27 @@
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
-export default class MyDocument extends Document {
-	static async getInitialProps(ctx) {
+class MyDocument extends Document {
+	static getInitialProps({ renderPage }) {
 		const sheet = new ServerStyleSheet();
-		const originalRenderPage = ctx.renderPage;
+		const page = renderPage(App => props =>
+			sheet.collectStyles(<App {...props} />)
+		);
+		const styleTags = sheet.getStyleElement();
+		return { ...page, styleTags };
+	}
 
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-				});
-
-			const initialProps = await Document.getInitialProps(ctx);
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				),
-			};
-		} finally {
-			sheet.seal();
-		}
+	render() {
+		return (
+			<Html>
+				<Head />
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
 	}
 }
+
+export default MyDocument;
